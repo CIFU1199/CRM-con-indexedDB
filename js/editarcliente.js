b348ec74f5f4b241667e09d1f030c27ee0b71afe
CIFU1199 <edwkamilo@gmail.com>
2022-07-20 13:40:19 -0500

    primer commit

---
+++ js/editarcliente.js
@@ -0,0 +1,79 @@
+(function(){
+    let DB;
+    const nombreInput = document.querySelector('#nombre');
+    const emailInput = document.querySelector('#email');
+    const telefonoInput = document.querySelector('#telefono');
+    const empresaInput = document.querySelector('#empresa');
+
+    const formulario = document.querySelector('#formulario');
+
+    document.addEventListener('DOMContentLoaded', ()=>{
+        conectarDB();      
+        //actualiza el registro
+        formulario.addEventListener('submit',actualizarCliente);
+
+        //verificar el ID de la url 
+        const parametrosURL = new URLSearchParams(window.location.search);
+        const idCliente = parametrosURL.get('id');
+        if(idCliente){
+            setTimeout(() => {
+                obtenerCliente(idCliente); 
+            }, 100);
+        }
+    });
+
+    function actualizarCliente(e){
+        e.preventDefault();
+        if(nombreInput.value === '' || emailInput.value ==='' || telefonoInput.value === '' || empresaInput.value === ''){
+            imprimirAlerta('Todos los campos son obligatorios', 'error');
+            return;
+        }
+
+        //actualizar cliente 
+        const clienteActualizado = {
+            
+        }
+    }
+
+    function obtenerCliente(id){
+        const transaction = DB.transaction(['crm'], 'readwrite');
+        const objectStore = transaction.objectStore('crm');
+
+        const cliente = objectStore.openCursor();
+        cliente.onsuccess = function(e) {
+            const cursor = e.target.result;
+            if(cursor){
+                if(cursor.value.id === Number(id)){
+                    llenarFormulario(cursor.value);
+                }
+                cursor.continue();
+            }
+        }
+        
+    }
+
+    function llenarFormulario(datosCliente){
+        const {nombre , email , telefono , empresa} = datosCliente;
+
+        nombreInput.value = nombre;
+        emailInput.value = email;
+        telefonoInput.value = telefono;
+        empresaInput.value = empresa;
+    }
+    
+    function conectarDB(){ 
+        // ABRIR CONEXIÓN EN LA BD:
+        let abrirConexion = window.indexedDB.open('crm', 1);
+
+        // si hay un error, lanzarlo
+        abrirConexion.onerror = function() {
+            console.log('Hubo un error');
+        };
+    
+        // si todo esta bien, asignar a database el resultado
+        abrirConexion.onsuccess = function() {
+            // guardamos el resultado
+            DB = abrirConexion.result;
+        };
+    }
+})();
